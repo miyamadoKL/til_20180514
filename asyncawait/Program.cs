6b627705c988016a2aceda2cfe3eea62548c20dd
miyamadoKL <105kansai500@gmail.com>
2018-05-15 06:18:17 +0900

    first commit

---
+++ asyncawait/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace asyncawait
+{
+    class Program
+    {
+        static async Task<int> CalculateAsync()
+        {
+            Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId}: CalculateAsync START");
+            var task = Task.Run(new Func<int>(Calculate));
+            Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId}: CalculateAsync Before");
+            var result = await task;
+            Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId}: CalculateAsync After");
+            return result;
+        }
+        static int Calculate()
+        {
+            Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId}: Calculate START");
+            int total = 0;
+            for (int i = 1; i <= 100; ++i)
+                total += i;
+            Thread.Sleep(2345);
+            Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId}: Calculate END");
+            return total;
+        }
+        static void Main(string[] args)
+        {
+            var task = CalculateAsync();
+            var result = task.Result;
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ lock/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace locktest
+{
+    class Program
+    {
+        static void inappropriateParallel()
+        {
+            const int ThreadNum = 20;
+            const int LoopNum = 20;
+            int num = 0;
+
+            Parallel.For(0, ThreadNum, i =>
+            {
+                for (int j = 0; j < LoopNum; j++)
+                {
+                    int tmp = num;
+                    Thread.Sleep(1);
+                    num = tmp + 1;
+                }
+            });
+            Console.WriteLine("inappropriate result");
+            Console.WriteLine("{0} ({1})\n", num, ThreadNum * LoopNum);
+        }
+        static void exclusiveParallel()
+        {
+            const int ThreadNum = 20;
+            const int LoopNum = 20;
+            int num = 0;
+
+            var syncObject = new object();
+
+            Parallel.For(0, ThreadNum, i =>
+            {
+                for (int j = 0; j < LoopNum; j++)
+                {
+                    lock (syncObject)
+                    {
+                        int tmp = num;
+                        Thread.Sleep(1);
+                        num = tmp + 1;
+                    }
+                }
+            });
+            Console.WriteLine("appropriate result");
+            Console.WriteLine("{0} ({1})\n", num, ThreadNum * LoopNum);
+        }
+        static void Main(string[] args)
+        {
+            inappropriateParallel();
+            exclusiveParallel();
+        }
+    }
+}
